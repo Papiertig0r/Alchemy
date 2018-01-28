e768e7a6de791bf5b312a5102c5b5940b2b3379d
Papiertig0r <christopher.putz@sheep-records.de>
2018-01-28 21:10:58 +0100

    Refactored consumables - each consumable will have their own buff added three potatoes to test item stacking

--- Assets/Scripts/Ingredients/Consumable.cs
+++ Assets/Scripts/Ingredients/Consumable.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 public class Consumable : MonoBehaviour, IConsumable, IShowable, IComparable
 {
     public string description;
-    public float strength = 10f;
-    public float concentration = 10f;
-    public float purity = 10f;
-    public float yield = 10f;
     public List<Buff> buffs = new List<Buff>();
 
     public void Consume(CharaController target)
@@ -16,9 +12,9 @@ public class Consumable : MonoBehaviour, IConsumable, IShowable, IComparable
         foreach(Buff buff in buffs)
         {
             Buff newBuff = Instantiate(buff);
-            newBuff.Set(strength, concentration, purity, yield);
             newBuff.Apply(target.stats);
         }
+        Destroy(this.gameObject);
     }
 
     public void DeactivateExtendedInfo()
