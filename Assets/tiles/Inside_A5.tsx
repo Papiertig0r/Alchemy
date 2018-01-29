be0d81da754efd16a37bed144f31a0fe10c7c64e
Papiertig0r <christopher.putz@sheep-records.de>
2018-01-29 19:23:03 +0100

    First failed attempt at building a map

---
+++ Assets/tiles/Inside_A5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Inside_A5" tilewidth="48" tileheight="48" tilecount="128" columns="8">
+ <image source="Inside_A5.png" width="384" height="768"/>
+</tileset>
